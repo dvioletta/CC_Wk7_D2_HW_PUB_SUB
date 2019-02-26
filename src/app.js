d5c7f1f5b91d3ffaa18f5ea66a55316b357e9124
dvioletta <Anita@Pixie.local>
2019-02-26 16:24:45 +0000

    set up files

---
+++ src/app.js
@@ -0,0 +1,9 @@
+const instrumentFamilyData = require('./data/instrument_family_data.js');
+const InstrumentFamilies = require('./models/instrument_families.js');
+const InstrumentsMenuView = require('./views/instruments_menu_view.js');
+const InstrumentsInfoView = require('./views/instruments_info_view.js');
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('JavaScript Loaded');
+});
---
+++ src/models/instrument_families.js
@@ -0,0 +1,7 @@
+const PubSub = require('../helpers/pub_sub.js');
+
+const InstrumentFamilies = function(data) {
+  this.data = data;
+};
+
+module.exports = InstrumentFamilies;
