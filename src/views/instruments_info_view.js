2c0648fb9119b538de1f0ac7ec82561c1382fb05
dvioletta <Anita@Pixie.local>
2019-02-26 22:10:37 +0000

    Info view

--- src/views/instruments_info_view.js
+++ src/views/instruments_info_view.js
@@ -4,5 +4,30 @@ const InstrumentsInfoView = function(container){
   this.container = container;
 }
 
+InstrumentsInfoView.protoytpe.bindEvents = function(){
+  PubSub.subscribe('Instruments:selected-instrument-ready', (evt) => {
+    const instrument = evt.detail;
+    this.renter(instrument);
+  });
+};
+
+InstrumentsInfoView.protoytpe.render = function(instrument){
+  this.container.innerHTML = ' ';
+
+  const heading = this.createHeading(instrument);
+  const infoList = this.createInfoList(instrument);
+
+  this.container.appendChild(heading);
+  this.container.appendChild(infoList);
+};
+
+PlanetInfoView.prototype.createHeading = function(instrument) {
+  const heading = document.createElement('h2');
+  heading.textContent = instrumentFamilyData.name;
+  return heading;
+};
+
+
+
 
 module.exports = InstrumentsInfoView;
